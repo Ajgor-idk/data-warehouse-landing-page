3687dea84c5c3d03a120ca8d83ebbc335669b737
Igor Owczarz <98612273+Ajgor-idk@users.noreply.github.com>
2022-07-23 19:31:55 +0200

    Added working slider with working JS thanks to swiper.js. Many changes because of that in HTML. Styled it in the CSS. Last things to do are the animations for buttons etc.

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  direction: "horizontal",
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
